a1658d9c19d400b5177ccdf7cf32e708b7df46b7
Kate1999 <kate.kisel99@yandex.by>
2021-05-19 23:13:44 +0300

    3

--- src/homework01/task01/LastNumber.java
+++ src/homework01/task01/LastNumber.java
@@ -8,12 +8,12 @@ public class LastNumber {
 
 		double x;
 		while (true) {
-			x = Instruments.inputDouble("��������������������� ���������������: ");
-			System.out.println("��������������������������� ��������������� ������������������������: " + getlastNumberSquare(x));
+			x = Instruments.inputDouble("Введите число: ");
+			System.out.println("Последняя цифра квадрата числа: " + getlastNumberSquare(x));
 		}
 	}
 
-	private static boolean isDouble(double x) { 
+	static boolean isDouble(double x) { 
 		return x % 1 != 0;
 	}
 
